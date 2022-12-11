9f38a722e3776f8e48c686417c0f48d1b3c9dc52
OlenaMurza <murzaelena@gmail.com>
2022-12-11 22:02:38 +0200

    push

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const divGallery = document.querySelector('gallery');
+// const divGallery = document.querySelector('gallery');
+console.log(galleryItemsJs);
+function galleryItemsJs (images) {
+    const items = images.map(image => {
+        return `
+           <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>
+        `
+    }).join('');
+}
 
 
-
-
-console.log(galleryItems);
+// console.log(items);
