2458bc946485acc114b0e429864e1d9d79817ed7
OlenaMurza <murzaelena@gmail.com>
2022-12-15 22:47:49 +0200

    push

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// const divGallery = document.querySelector('gallery');
-console.log(galleryItemsJs);
-function galleryItemsJs (images) {
-    const items = images.map(image => {
+const divGallery = document.querySelector(".gallery");
+
+console.log(createGalleryLayout(galleryItems));
+function createGalleryLayout(galleryItems) {
+  return galleryItems
+    .map(({preview, original, description}) => {
         return `
            <div class="gallery__item">
-      <a class="gallery__link" href="large-image.jpg">
+      <a class="gallery__link" href="$(original)">
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src="$(preview)"
+      data-source="$(original)"
+      alt="$(description)"
     />
   </a>
 </div>
         `
     }).join('');
 }
+
+divGallery.addEventListener("click", (e) => {
+  e.preventDefault(); if (e.target.nodeName !== "IMG") {
+    return;
+
+  }
+  // Якщо виводиться картинка
+  const urlBigImage = e.target.getAttribute("data-source");
+  const picture = basicLingthbox.create(`<img src ="${urlBigImage}" width = "800" heigth = "600">`);
+  picture.show();
+});
+
+
 
 
 // console.log(items);
