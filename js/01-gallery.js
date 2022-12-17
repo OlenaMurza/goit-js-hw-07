ac892a9d8267e1f31f87c139e570bd874f344b1f
OlenaMurza <murzaelena@gmail.com>
2022-12-17 22:24:07 +0200

    push

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const divGallery = document.querySelector(".gallery");
+const divGallery = document.querySelector('.gallery');
 
-console.log(createGalleryLayout(galleryItems));
+// console.log(createGalleryLayout(galleryItems));
 function createGalleryLayout(galleryItems) {
   return galleryItems
-    .map(({preview, original, description}) => {
-        return `
-           <div class="gallery__item">
-      <a class="gallery__link" href="$(original)">
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="$(preview)"
-      data-source="$(original)"
-      alt="$(description)"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
-</div>
-        `
-    }).join('');
+</div>`;
+    })
+    .join('');
 }
 
-divGallery.addEventListener("click", (e) => {
-  e.preventDefault(); if (e.target.nodeName !== "IMG") {
-    return;
+divGallery.insertAdjacentHTML('afterbegin', createGalleryLayout(galleryItems));
 
+divGallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
   }
   // Якщо виводиться картинка
-  const urlBigImage = e.target.getAttribute("data-source");
-  const instance = basicLingthbox.create(`<img src ="${preview}" width = "800" heigth = "600">`);
+  const urlBigImage = e.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src ="${urlBigImage}" width = "800" heigth = "600">`
+  );
+  
   instance.show();
-
-
 });
 
 
