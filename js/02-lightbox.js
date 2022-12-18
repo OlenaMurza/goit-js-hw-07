74a5477942987f64d56a6c01198cc06c8967dd16
OlenaMurza <murzaelena@gmail.com>
2022-12-18 09:14:58 +0200

    push

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divGallery = document.querySelector('.gallery');
+
+// console.log(createGalleryLayout(galleryItems));
+function createGalleryLayout(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="large-image.jpg" onclick = "return false">
+  <img class="gallery__image" src="${preview}"
+      data-source="${original}"
+      alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+divGallery.insertAdjacentHTML('afterbegin', createGalleryLayout(galleryItems));
+
+divGallery.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  // Якщо виводиться картинка
+  const urlBigImage = e.target.dataset.source;
+  const instance = simpleLightbox.create(
+    `<img src ="${urlBigImage}" width = "800" heigth = "600">`
+  );
+  
+    instance.show();
+    
+    let gallery = new simpleLightbox('.gallery a', {
+        capsionsData: 'alt', captionPosition: 'bottom', captionDelay: 250,
+    });
+    gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+});
+
+
+
+console.log(simpleLightbox);
